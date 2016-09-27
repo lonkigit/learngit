37af4d9e9863208a70df91852d25a21081f96331
lonki <zhujieworkroom@126.com>
2016-09-27 15:55:30 +0800

    编写配置文件

---
+++ lonki_python3_webapp/www/config_default.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'lonki'
+
+configs = {
+    'debug':True,
+    'db':{
+        'host':'127.0.0.1',
+        'port':3306,
+        'user':'root',
+        'password':'zhujie8721*',
+        'db':'project'
+    },
+    'session':{
+        'secret','lonki'
+    }
+}
