60fabdbf57c8d55ae42957b59b68a06430d8b103
lonki <zhujieworkroom@126.com>
2016-10-10 18:29:56 +0800

    scrapy stady

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -38,4 +38,67 @@ class DoubanSpider(scrapy.Spider):
             f.write(response.body)
         self.log('Save file %s' %filename)
 
+class QuotesSpider2(scrapy.Spider):
+    name = "quotes2"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
 
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text' : quote.css("span.text::text").extract_first(),
+                'author' : quote.css("small.author::text").extract_first(),
+                'tags' : quote.css("div.tags a.tag::text").extract(),
+            }
+            print(dict(text=quote.css("span.text::text").extract_first(), author= quote.css("small.author::text").extract_first(), tags= quote.css("div.tags a.tag::text").extract()))
+
+
+class QuotesSpider3(scrapy.Spider):
+    name = "quotes3"
+    start_urls = [
+        'http://quotes.toscrape.com／page/1',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css("span.text::text").extract_first(),
+                'author': quote.css("small.author::text").extract_first(),
+                'tags': quote.css("div.tags a.tag::text").extract(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page,callback=self.parse)
+
+
+class AuthorSpider(scrapy.Spider):
+    name = 'author'
+
+    start_urls = ['http://quotes.toscrape.com']
+
+    def parse(self, response):
+        #follow links to author pages
+        for href in response.css('.author a::attr(href)').extract():
+            yield scrapy.Request(response.urljoin(href),callback=self.parse_author)
+
+
+        #follow pagination links
+            next_page = response.css('li.next a::attr(href)').extract_first()
+            if next_page is not None:
+                next_page = response.urljoin(next_page)
+                yield scrapy.Request(next_page,callback=self.parse)
+
+
+    def parse_author(self,response):
+        def extract_with_css(query):
+            return response.css(query).extract_first().strip()
+
+        yield {
+            'name' : extract_with_css('h3.author-title::text'),
+            'birthdate' : extract_with_css('.author-born-date::text'),
+            'bio' : extract_with_css('.author-description::text')
+        }
